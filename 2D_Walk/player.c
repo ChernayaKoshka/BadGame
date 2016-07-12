5ab0dcb13c35103f1e5e925ea95dc8a8e2735639
Christopher A <chris.altig@gmail.com>
2016-07-11 22:30:03 -0700

    Fixed player collision by added four collision points instead of the one

--- 2D_Walk/player.c
+++ 2D_Walk/player.c
@@ -1,49 +1,74 @@
 #include "player.h"
-#include "globals.h"
-#include "screen.h"
 
+#if DISPLAY_COLLISION_PROCESS
 extern POINT dbgTilePos;
 extern WindowDetails* details;
 extern TileInfo* dbgTile;
+#endif
+
+void killPlayer(Player* player)
+{
+	player->pos.x = 0;
+	player->pos.y = 200;
+}
 
 BOOL changePos(Player* player, Level* level, int newX, int newY)
 {
+	POINT newPos = { 0 };
+	newPos.x = newX;
+	newPos.y = newY;
+
 	for (int i = 0; i < 4; i++)
 	{
 		POINT colPoint = player->collisionPoints[i];
 		if (colPoint.y + newY >= level->levelHeight || colPoint.y + newY < 0 || newX > level->levelWidth - SPRITE_WIDTH || newX < 0)
 			return TRUE;
-		int test = roundUpTo(newY - colPoint.y, SPRITE_HEIGHT);
-		int test2 = roundUpTo(newX - colPoint.x, SPRITE_WIDTH);
 
-		dbgTilePos.y = test;
-		dbgTilePos.x = test2;
+		for (int j = 0; j < level->entityCount; j++)
+		{
+			POINT* entityPoints = getEntityPoints(level, level->entities[j].entityId);
+			if (!rectContainsPoint(entityPoints, newPos))
+			{
+				free(entityPoints);
+				continue;
+			}
+			if (level->entities[j].is_dangerous)
+			{
+				free(entityPoints);
+				killPlayer(player);
+				return FALSE;
+			}
+			free(entityPoints);
+		}
 
-		writeTile(details->BackBuffer, details->Width, dbgTilePos.x, dbgTilePos.y, dbgTile);
+		int adjustedY = roundUpTo(newY - colPoint.y, SPRITE_HEIGHT);
+		int adjustedX = roundUpTo(newX - colPoint.x, SPRITE_WIDTH);
+
+#if DISPLAY_COLLISION_PROCESS
+		dbgTilePos.y = adjustedY;
+		dbgTilePos.x = adjustedX;
+
+		writeBitmap(details->BackBuffer, details->Width, dbgTilePos.x, dbgTilePos.y, dbgTile->bitmap);
 		StretchDIBits(details->DC,
 			0, 0, details->Width, details->Height,
 			0, 0, details->BitMapInfo.bmiHeader.biWidth, Abs(details->BitMapInfo.bmiHeader.biHeight),
 			details->BackBuffer, &details->BitMapInfo,
 			DIB_RGB_COLORS, SRCCOPY);
-		Sleep(250);
+		Sleep(50);
+#endif
 
-		TileInfo* info = getTileAtPos(level, test, test2);
+		TileInfo* info = getTileAtPos(level, adjustedY, adjustedX);
 		if (info->is_collidable)
 			return TRUE;
 	}
 	player->pos.x = newX;
 	player->pos.y = newY;
+	return FALSE;
 }
 
 TileInfo* getTileUnderPlayer(Player* player, Level* level)
 {
 	return getTileAtPos(level, player->pos.y + SPRITE_HEIGHT, player->pos.x);
-}
-
-void killPlayer(Player* player)
-{
-	player->pos.x = 0;
-	player->pos.y = 0;
 }
 
 void updatePlayer(Player* player, Level* level, unsigned long step)
--- 2D_Walk/player.h
+++ 2D_Walk/player.h
@@ -1,7 +1,11 @@
 #pragma once
+
 #include <Windows.h>
 #include "math_custom.h"
 #include "level.h"
+#include "globals.h"
+#include "screen.h"
+#include "entity.h"
 
 #define PLAYER_JUMP_HEIGHT SPRITE_HEIGHT*2
 
@@ -18,14 +22,6 @@ typedef struct tagPlayer
 	int jumpHeight;
 	unsigned int isJumping : 1;
 }Player;
-
-typedef enum tageCollisionLocation
-{
-	TOPLEFT = 0,
-	BOTTOMLEFT,
-	TOPRIGHT,
-	BOTTOMRIGHT
-}eCollisionLocation;
 
 BOOL changePos(Player* player, Level* level, int newX, int newY);
 
